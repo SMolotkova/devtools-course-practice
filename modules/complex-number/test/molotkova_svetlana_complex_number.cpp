440d1e02673589cc782d38799eadcb45039648b1
SMolotkova <svetlanamolotkova2000@gmail.com>
2021-03-15 12:59:12 +0300

    Updated tests

--- modules/complex-number/test/molotkova_svetlana_complex_number.cpp
+++ modules/complex-number/test/molotkova_svetlana_complex_number.cpp
@@ -8,7 +8,7 @@ TEST(Molotkova_Svetlana_ComplexNumberTesting, Pow) {
     ComplexNumber a1(2.0, 3.0);
     ComplexNumber a2(a1);
     ComplexNumber rres = a1*a2;
-    ComplexNumber eres(-5.0,12.0);
+    ComplexNumber eres(-5.0, 12.0);
     EXPECT_EQ(rres.getRe(), eres.getRe());
     EXPECT_EQ(rres.getIm(), eres.getIm());
 }
@@ -17,13 +17,13 @@ TEST(Molotkova_Svetlana_ComplexNumberTesting, Multiply_by_number) {
     ComplexNumber b1(3.0, 0);
     double b2 = 3.0;
     ComplexNumber rres = a*b1;
-    ComplexNumber eres(a.getRe()*b2,a.getIm()*3);
+    ComplexNumber eres(a.getRe()*b2, a.getIm()*3);
     bool res = rres == eres;
     EXPECT_TRUE(res);
 }
 
 TEST(Molotkova_Svetlana_ComplexNumberTesting, No_Exeption) {
     ComplexNumber a1(2.0, 9.0);
-	ComplexNumber a2(3.0, 3.0);
-	ASSERT_NO_THROW(a1 / a2);
+    ComplexNumber a2(3.0, 3.0);
+    ASSERT_NO_THROW(a1 / a2);
 }
