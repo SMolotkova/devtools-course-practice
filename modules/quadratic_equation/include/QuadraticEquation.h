5779409ac10802c7a020cea779a9b4dd0fe97e66
SMolotkova <svetlanamolotkova2000@gmail.com>
2021-04-22 20:18:56 +0300

    quadratic_equation2

--- modules/quadratic_equation/include/QuadraticEquation.h
+++ modules/quadratic_equation/include/QuadraticEquation.h
@@ -10,7 +10,7 @@ class QuadraticEquation {
     double a;
     double b;
     double c;
-    QuadraticEquation(double a, double b, double c){
+    QuadraticEquation(double a, double b, double c) {
     this->a = a;
     this->b = b;
     this->c = c;
