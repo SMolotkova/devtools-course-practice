f816d80715b7e531563c59658207d301b8dea779
Svetlana Molotkova <42940205+SMolotkova@users.noreply.github.com>
2021-04-25 08:59:58 +0300

    Update QuadraticEquation.cpp

--- modules/quadratic_equation/src/QuadraticEquation.cpp
+++ modules/quadratic_equation/src/QuadraticEquation.cpp
@@ -5,6 +5,16 @@
 
 #include "include/QuadraticEquation.h"
 
+QuadraticEquation::QuadraticEquation(double a, double b, double c) {
+    if (a == 0){
+         throw "Not quadratic equation";
+     } else {
+       this->a = a;
+       this->b = b;
+       this->c = c;
+     }
+  }
+
 double QuadraticEquation::discr(double a, double b, double c) {
     double d = b * b - 4 * a * c;
     return d;
@@ -13,7 +23,7 @@ double QuadraticEquation::discr(double a, double b, double c) {
 std::pair<double, double> QuadraticEquation::solver() {
     double d = discr(a, b, c);
     if (d < 0) {
-         throw "Error";
+         throw "The discriminate value is less than zero";
      } else {
         double x1 = (-b + sqrt(d)) / (2 * a);
         double x2 = (-b - sqrt(d)) / (2 * a);
